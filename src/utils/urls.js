ebca55964a5d99304549dbf19354cfb43aca9b67
intehon <intehon@gmail.com>
2023-09-16 22:47:33 +0200

    change url for heroku

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,11 +1,11 @@
-export const WINE_URL = `https://winemag.herokuapp.com/wines?page=1&limit=8000`
+export const WINE_URL = `https://winemag-b2751d269215.herokuapp.com/wines?page=1&limit=8000`
 
-export const TITLE_URL = (wineTitle) => `https://winemag.herokuapp.com/wines/titles/${wineTitle}`
+export const TITLE_URL = (wineTitle) => `https://winemag-b2751d269215.herokuapp.com/wines/titles/${wineTitle}`
 
-export const TOP_URL = `http://winemag.herokuapp.com/wines/top-rated`
+export const TOP_URL = `https://winemag-b2751d269215.herokuapp.com/wines/top-rated`
 
-export const PRICE_URL = `http://winemag.herokuapp.com/wines/most-expensive`
+export const PRICE_URL = `https://winemag-b2751d269215.herokuapp.com/wines/most-expensive`
 
-export const SELECT_URL = (select) => `http://winemag.herokuapp.com/wines/${select}`
+export const SELECT_URL = (select) => `https://winemag-b2751d269215.herokuapp.com/wines/${select}`
 
-export const VARIETY_URL = (wineVariety) => `https://winemag.herokuapp.com/wines/varieties/${wineVariety}`
+export const VARIETY_URL = (wineVariety) => `https://winemag-b2751d269215.herokuapp.com/wines/varieties/${wineVariety}`
